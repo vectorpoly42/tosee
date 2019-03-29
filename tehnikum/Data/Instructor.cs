8172a6219b67ceee9caeb4b5b84d17b031ab21ea
Рылеев Денис <ultimate.pelemewka@gmail.com>
2019-03-29 15:11:18 +0300

    Добавьте файлы проекта.

---
+++ tehnikum/Data/Instructor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ContosoUniversity.Data
+{
+    internal class Instructor
+    {
+        public string FirstMidName { get; set; }
+        public string LastName { get; set; }
+        public DateTime HireDate { get; set; }
+    }
+}
